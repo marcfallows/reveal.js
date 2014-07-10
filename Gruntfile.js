e214389a24fa52dd0a8bc2e941bd4e85f3ac73d5
marc <marc@blinkbox.com>
2014-07-10 18:14:52 +0100

    Seed for blinkbox presentations

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,6 +41,7 @@ module.exports = function(grunt) {
 			main: {
 				files: {
 					'css/theme/default.css': 'css/theme/source/default.scss',
+					'css/theme/blinkbox.css': 'css/theme/source/blinkbox.scss',
 					'css/theme/beige.css': 'css/theme/source/beige.scss',
 					'css/theme/night.css': 'css/theme/source/night.scss',
 					'css/theme/serif.css': 'css/theme/source/serif.scss',
@@ -80,7 +81,8 @@ module.exports = function(grunt) {
 			server: {
 				options: {
 					port: port,
-					base: '.'
+					base: '.',
+                    hostname: '*'
 				}
 			}
 		},
